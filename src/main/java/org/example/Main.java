70e6d74589de0bebc3aa20591b5e8f9c027729ae
Данил Токарев <danil2002tokarev@mail.ru>
2023-08-06 15:52:20 +0300

    end

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,19 @@
 package org.example;
 
-import org.example.eds.file.pkcs.KeyStorePKCS12;
-import org.example.signature.ECDSA.SignatureECDSA;
-import java.security.PrivateKey;
+
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String fileName1 = "test_for_doc.docx";
-        String fileSignName1 = "sign_doc_file.docx";
-        String passwordKeyStore1 = "java";
         String ellipticCurve = "secp521r1";
-        String alias1 = "first name: ivan\n last name: ivanov\n " +
-                "patronymic: ivanovich\n mail: example@mail.ru\n tel: +79000000000";
-        String fileKeyStore1 = "DOCXkey.p12";
-        RequestEDS.signDocxFile(fileName1, fileSignName1, passwordKeyStore1, ellipticCurve, alias1, fileKeyStore1);
-
-        System.out.println(RequestEDS.verifySignDocxFile(fileSignName1,fileKeyStore1, passwordKeyStore1));
-
-
-
-
         String fileName = "test_for_pdf.pdf";
         String fileSignName = "sign_pdf_file.pdf";
         String passwordKeyStore = "java";
         String alias = "first name: ivan\n last name: ivanov\n " +
                 "patronymic: ivanovich\n mail: example@mail.ru\n tel: +79000000000";
         String fileKeyStore = "PDFkey.p12";
-        String fileSignName2 = "sign_pdf_file2.pdf";
 
-//        RequestEDS.signPDFFile(fileName, fileSignName, passwordKeyStore, fileKeyStore);
-//        System.out.println(RequestEDS.verifySignPDFFile(fileSignName, fileKeyStore, passwordKeyStore));
-//
         RequestEDS.signPDFFile(fileName, fileSignName, passwordKeyStore, ellipticCurve, alias, fileKeyStore);
-
-
-
         System.out.println(RequestEDS.verifySignPDFFile(fileSignName, fileKeyStore, passwordKeyStore));
-//
-//        RequestEDS.signPDFFile(fileSignName, fileSignName2, passwordKeyStore, ellipticCurve, alias, fileKeyStore);
-//
-//
-//
-//        System.out.println(RequestEDS.verifySignPDFFile(fileSignName2, fileKeyStore, passwordKeyStore));
 
     }
 }
--- src/main/java/org/example/RequestEDS.java
+++ src/main/java/org/example/RequestEDS.java
@@ -2,7 +2,6 @@ package org.example;
 
 import com.itextpdf.text.DocumentException;
 import org.bouncycastle.operator.OperatorCreationException;
-import org.example.eds.file.DOCX.SignatureDOCX;
 import org.example.eds.file.PDF.SignaturePDF;
 import org.example.eds.file.pkcs.KeyStorePKCS12;
 import org.example.signature.ECDSA.SignatureECDSA;
@@ -12,117 +11,6 @@ import java.security.cert.CertificateException;
 import java.util.Base64;
 
 public class RequestEDS {
-
-    //    Подписание файла в формате ".docx" с генерацией нового сертификата электронной подписи
-//    fileName - имя подписываемого файла ".docx"
-//    fileSignName - имя создаваемого файла ".docx" с ЭП
-//    passwordKeyStore - пароль для сертификата ЭП
-//    ellipticCurve - эллиптическая кривая (P-256, P-384, P-521), на основе которой будет создаваться ЭП
-//    alias - информация о владельце ЭП
-//    fileKeyStore - файл для сохранения сертификата электронной подписи ".р12"
-    public static void signDocxFile(String fileName, String fileSignName, String passwordKeyStore, String ellipticCurve, String alias, String fileKeyStore) throws Exception {
-//      создание объекта класса SignatureDOCX
-        SignatureDOCX signatureDOCX = new SignatureDOCX();
-//      открытие подписываемого файла
-        signatureDOCX.unArchive(fileName);
-//      чтение содержимого файла
-        signatureDOCX.readFile(fileName);
-
-//      создание объекта класса SignatureECDSA
-        SignatureECDSA sig = new SignatureECDSA();
-//      добавление криптопровайдера Bouncy castle
-        sig.setSecurityProvider();
-//      создание ключевой пары
-        KeyPair kp = sig.generateKeys(ellipticCurve);
-//      создание ЭП на основе файла ".docx" и информации о подписывающем
-        byte[] s = sig.generateSignature(signatureDOCX.getContent() + alias, kp.getPrivate());
-//      создание нового сертификата ЭП
-        KeyStorePKCS12.savePKCS12(KeyStorePKCS12.generatePKCS12KeyStore(passwordKeyStore, kp, alias), fileKeyStore, passwordKeyStore);
-//      подписание документа ".docx"
-        signatureDOCX.signFile(Base64.getEncoder().encodeToString(s));
-//      сохранение нового файла ".docx" с ЭП
-        signatureDOCX.archive(fileName, fileSignName);
-    }
-
-    //    Подписание файла в формате ".docx" на основе существующего сертификата ЭП ".р12"
-//    fileName - имя подписываемого файла ".docx"
-//    fileSignName - имя создаваемого файла ".docx" с ЭП
-//    passwordKeyStore - пароль для сертификата ЭП
-//    fileKeyStore - файл для сохранения сертификата электронной подписи ".р12"
-    public static void signDocxFile(String fileName, String fileSignName, String passwordKeyStore, String fileKeyStore) throws Exception {
-//      создание объекта класса keyStorePKCS12
-        KeyStorePKCS12 keyStorePKCS12 = new KeyStorePKCS12(fileKeyStore, passwordKeyStore);
-//      проверка сертификата ЭП на дествительность по дате
-        if (keyStorePKCS12.validData()) {
-//          создание объекта класса SignatureDOCX
-            SignatureDOCX signatureDOCX = new SignatureDOCX();
-//          открытие подписываемого файла
-            signatureDOCX.unArchive(fileName);
-//          чтение содержимого файла
-            signatureDOCX.readFile(fileName.replace(".docx", ""));
-//          создание объекта класса SignatureECDSA
-            SignatureECDSA sig = new SignatureECDSA();
-//          добавление криптопровайдера Bouncy castle
-            sig.setSecurityProvider();
-//          получение секретного ключа из сертификата ЭП
-            PrivateKey privateKey = keyStorePKCS12.getPrivateKey();
-//          получение информации о владельце сертификата ЭП
-            String alias = keyStorePKCS12.getAlias();
-//          формирование информации, которую требуется подписать
-            String content = signatureDOCX.getContent() + alias;
-//          создание ЭП
-            byte[] s = sig.generateSignature(content, privateKey);
-//          подписание документа ".docx"
-            signatureDOCX.signFile(Base64.getEncoder().encodeToString(s));
-//          сохранение нового файла ".docx" с ЭП
-            signatureDOCX.archive(fileName.replace(".docx", ""), fileSignName);
-        } else {
-//          сообщение о том, чтог сертификат не действителен по дате
-            System.out.println("Certificate is is not valid by date");
-        }
-    }
-
-    //  Проверка ЭП в файле ".docx"
-//  fileName - подписанный файл
-//  keyStore - файл с ертификатом ЭП
-//  password - пароль от сертификата ЭП
-    public static String verifySignDocxFile(String fileName, String keyStore, String password) throws IOException, CertificateException, KeyStoreException, NoSuchAlgorithmException, SignatureException, InvalidKeyException, NoSuchProviderException, UnrecoverableKeyException {
-//      создание объекта класса keyStorePKCS12
-        KeyStorePKCS12 keyStorePKCS12 = new KeyStorePKCS12(keyStore, password);
-//      проверка сертификата ЭП на дествительность по дате
-        if (keyStorePKCS12.validData()) {
-//          создание объекта класса SignatureDOCX
-            SignatureDOCX signatureDOCX = new SignatureDOCX();
-//          получение ЭП из подписанного файла ".docx"
-            String signature = signatureDOCX.getSignatureFromFile(fileName);
-            byte[] sign = Base64.getDecoder().decode(signature);
-//          получение информации о владельце сертификата ЭП
-            String alias = keyStorePKCS12.getAlias();
-//          формирование информации, которую подписали данной ЭП
-            String content = signatureDOCX.getContentWithoutSignature(signature) + alias;
-//          получение открытого ключа из сертификата ЭП
-            PublicKey publicKey = keyStorePKCS12.getPublicKeyFrom();
-//          создание объекта класса SignatureECDSA
-            SignatureECDSA signatureECDSA = new SignatureECDSA();
-//          добавление криптопровайдера Bouncy castle
-            signatureECDSA.setSecurityProvider();
-//          получение результата проверки ЭП
-            boolean result = signatureECDSA.validateSignature(content, publicKey, sign);
-//          формирование сообщения о результате проверки
-            StringBuilder res = new StringBuilder();
-            if (result) {
-                res.append(alias);
-                res.append("\nSignature is valid");
-            } else {
-                res.append("\nSignature is invalid or data in the document has been changed");
-            }
-
-            return res.toString();
-        } else {
-//      сообщение о том, что сертификат ЭП не действителен по времени
-            return "The signature is not valid by date";
-        }
-    }
 
     //    Подписание файла в формате ".pdf" с генерацией нового сертификата электронной подписи
 //    fileName - имя подписываемого файла ".docx"
